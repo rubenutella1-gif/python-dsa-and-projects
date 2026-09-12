be490288716cc3ac919ff070a839afe2c789b787
Rubenu Tella <rubenutella1@gmail.com>
2026-09-12 20:58:45 +0530

    Added code

---
+++ Python/ARRAY_DSA_Problems/two_pointer_sum3.py
@@ -0,0 +1,24 @@
+nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
+nums.sort()
+result = []
+for i in range(len(nums) - 2):
+    if i > 0 and nums[i] == nums[i - 1]:
+        continue
+    left = i + 1
+    right = len(nums) - 1
+    while left < right:
+        total = nums[i] + nums[left] + nums[right]
+        if total == 0:
+            result.append([nums[i], nums[left], nums[right]])
+            left += 1
+            right -= 1
+            while left < right and nums[left] == nums[left - 1]:
+                left += 1
+            while left < right and nums[right] == nums[right + 1]:
+                right -= 1
+        elif total < 0:
+            left += 1
+        else:
+            right -= 1
+print(nums)
+print("Triplets:", result)
